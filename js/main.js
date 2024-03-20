e37e0aae703d6177e83ac21fddf5f3da4b20f8b0
alan_omam <omamalan@hotmail.it>
2024-03-20 15:17:43 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            
+            /*
+            todoList: [],
+            newTodoText: '',*/
+        }
+    },
+
+    methods: {
+
+        /*addTodo() {
+            
+            axios.get('./server.php?newTodo=' + this.newTodoText).then(res => {
+                console.log(res);
+                this.todoList = res.data
+            })
+
+        }*/
+    },
+
+    mounted() {
+
+       /* axios.get('./server.php').then(res => {
+            console.log(res.data);
+
+            this.todoList = res.data;
+        });*/
+
+    },
+}).mount('#app');
